a4ef61a8bb948433a6d88e3648d05a4269ab74ca
Daniel Severo Alves <108952262+alvespinho@users.noreply.github.com>
2023-10-23 00:42:58 -0300

    Add files via upload

---
+++ prova1.py
@@ -0,0 +1,29 @@
+
+######### Tipagem de Dados:
+
+string = '12'
+integer = 12
+float = 12.0
+bool = True 
+
+######### Solicitação e Interação com Usuário:
+
+print ('Olá, Seja Bem Vindo!')
+name = input ('Digite seu nome: ')
+age = int (input ('Idade: '))
+city = input ('Cidade: ')
+state = input ('Estado: ')
+street = input ('Endereço: \n Rua: ')
+number = int (input ('\n Número: '))
+hood = input ('\n Bairro: ')
+zipcode = int (input ('\n CEP: '))
+
+print (F'Muito Obrigado, {name}, agora confira seus dados: \n Idade: {age} \n Cidade: {city} \n Estado: {state} \n Endereço: {street}, {number}, {hood} — CEP.: {zipcode}. {city}, {state}.')
+
+print ('Parabéns! Cadastro realizado com sucesso!')
+
+
+
+
+
+
