1c1a06c98282d47a12051ee53d257dd65c1a8503
Alves Pinho <108952262+alvespinho@users.noreply.github.com>
2023-10-28 22:50:04 -0300

    Update scriptsigno.js

--- scriptsigno.js
+++ scriptsigno.js
@@ -13,50 +13,50 @@ msg.innerHTML = `Agora são ${hora} horas.`
 
 if (mes == 2) {
     /// AQUARIO
-    img.src = "morning_pic.png"
+    img.src = ""
     document.body.style.background = ""
 } else if (mes == 3){
     /// PEIXES
-    img.src = "noon_pic.png"
+    img.src = ""
     document.body.style.background = ""
 } else if (mes == 4){
     /// ARIES
-    img.src = "noon_pic.png"
+    img.src = ""
     document.body.style.background = ""
 } else if (mes == 5){
     /// TOURO
-    img.src = "noon_pic.png"
+    img.src = ""
     document.body.style.background = ""
 } else if (mes == 6){
     /// GÊMEOS
-    img.src = "noon_pic.png"
+    img.src = ""
     document.body.style.background = ""
 } else if (mes == 7){
     /// CÂNCER 
-    img.src = "noon_pic.png"
+    img.src = ""
     document.body.style.background = ""
 } else if (mes == 8){
     /// LEAO
-    img.src = "noon_pic.png"
+    img.src = ""
     document.body.style.background = ""
 } else if (mes == 9){
     /// VIRGEM
-    img.src = "noon_pic.png"
+    img.src = ""
     document.body.style.background = ""
 } else if (mes == 10){
     /// LIBRA
-    img.src = "noon_pic.png"
+    img.src = ""
     document.body.style.background = ""
 } else if (mes == 11){
     /// ESCORPIÃO 
-    img.src = "noon_pic.png"
+    img.src = ""
     document.body.style.background =""
 } else if (mes == 12){
     /// SAGITÁRIO 
-    img.src = "noon_pic.png"
+    img.src = ""
     document.body.style.background = ""
 } else (mes == 1){
     /// CAPRICÓRNIO
-    img.src = "noon_pic.png"
+    img.src = ""
     document.body.style.background = ""
 }}
