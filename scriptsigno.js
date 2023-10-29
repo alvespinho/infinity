f5ce651dd590cd74542c18bd7e7a05c95ce1ffae
Alves Pinho <108952262+alvespinho@users.noreply.github.com>
2023-10-28 23:46:01 -0300

    Update scriptsigno.js

--- scriptsigno.js
+++ scriptsigno.js
@@ -1,63 +1,67 @@
 function confirmar() {
 
-let msg = window.document.getElementById ("msg")
+let msg = window.document.getElementById ("res")
 let img = window.document.getElementById ("imagem")
+
 let data = new Date()
 
 /// let hora = data.getHours()  << USAR HORA ATUAL >>
 
-let mes = data.getMonth()
-let dia = data.getDay()
+let mes = document.getElementById("txtano")
+let dia = document.getElementById("txtdia")
 
-/// let ano = data.fullYear()
 
-msg.innerHTML = `Agora são ${hora} horas.`
 
-if (mes == 2) {
+res.innerHTML = `Você é ${}.`
+
+
+if (mes.value > 12 || mes.value < 1 || dia.value < 1 || dia.value > 31) {
+    window.alert ("Erro. Data de Nascimento inválida.")
+} else if (mes.value == 2) {
     /// AQUARIO
     img.src = ""
     document.body.style.background = ""
-} else if (mes == 3){
+} else if (mes.value == 3){
     /// PEIXES
     img.src = ""
     document.body.style.background = ""
-} else if (mes == 4){
+} else if (mes.value == 4){
     /// ARIES
     img.src = ""
     document.body.style.background = ""
-} else if (mes == 5){
+} else if (mes.value == 5){
     /// TOURO
     img.src = ""
     document.body.style.background = ""
-} else if (mes == 6){
+} else if (mes.value == 6){
     /// GÊMEOS
     img.src = ""
     document.body.style.background = ""
-} else if (mes == 7){
+} else if (mes.value == 7){
     /// CÂNCER 
     img.src = ""
     document.body.style.background = ""
-} else if (mes == 8){
+} else if (mes.value == 8){
     /// LEAO
     img.src = ""
     document.body.style.background = ""
-} else if (mes == 9){
+} else if (mes.value == 9){
     /// VIRGEM
     img.src = ""
     document.body.style.background = ""
-} else if (mes == 10){
+} else if (mes.value == 10){
     /// LIBRA
     img.src = ""
     document.body.style.background = ""
-} else if (mes == 11){
+} else if (mes.value == 11){
     /// ESCORPIÃO 
     img.src = ""
     document.body.style.background =""
-} else if (mes == 12){
+} else if (mes.value == 12){
     /// SAGITÁRIO 
     img.src = ""
     document.body.style.background = ""
-} else (mes == 1){
+} else (mes.value == 1){
     /// CAPRICÓRNIO
     img.src = ""
     document.body.style.background = ""
