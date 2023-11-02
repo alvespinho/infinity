c4cf8ac104308a9e5219f59bdeff9c1e199fe184
Alves Pinho <108952262+alvespinho@users.noreply.github.com>
2023-11-02 15:09:47 -0300

    Update PY_PROVA4.py

--- PY_PROVA4.py
+++ PY_PROVA4.py
@@ -1,5 +1,5 @@
 list1 = (input('Insira uma lista de números separados por espaço e aperte enter: ').split(' '))
-list2 = ((input('Insira uma lista de números separados por espaço e aperte enter: ').split(' ')))
+list2 = ((input('Insira uma lista de números separados por espaço e aperte enter: ').split(' '))
 list1.extend(list2)
 print(list1)
 
@@ -7,7 +7,7 @@ print(list1)
 #### OUTRO JEITO POSSÍVEL ###
 
 # list1 = (input('Insira uma lista de números separados por espaço e aperte enter: ').split(' '))
-# list2 = ((input('Insira uma lista de números separados por espaço e aperte enter: ').split(' ')))
+# list2 = (input('Insira uma lista de números separados por espaço e aperte enter: ').split(' '))
 # for i in list2:
 #     list1.append(i)
 # print (list1)
