4c77dc683180ab2976e0026f8693606965586b68
Alves Pinho <108952262+alvespinho@users.noreply.github.com>
2023-11-14 02:15:20 -0300

    Update LP_PROVA5.py

--- LP_PROVA5.py
+++ LP_PROVA5.py
@@ -2,6 +2,8 @@ dados = []
 soma_idades = 0
 usuarios_below_20 = 0
 usuarios_above_40 = 0
+oldest_usuario = None
+oldest_idade = 0
 youngest_genero = None
 youngest_idade = 100 #### Referência de Idade
 
@@ -12,6 +14,11 @@ for i in range(5):
     
     soma_idades += idade
     dados.append({'Usuario': usuario, 'Idade': idade, 'Genero': genero})
+    
+    #### Atualizar valor da pessoa mais velha
+    if idade > oldest_idade:
+    	oldest_idade = idade 
+    	oldest_usuario = usuario
     
     #### Atualizar valor da idade caçula 
     if idade < youngest_idade:
@@ -26,6 +33,11 @@ for i in range(5):
 
 media_idades = int(soma_idades / len(dados))
 print(f'A média de idades é: {media_idades}')
-print(f'O gênero do usuário mais jovem é: {youngest_genero}')
+
+print(f'O usuário mais velho se chama: {oldest_usuario}')
+
 print(f'Número de usuários abaixo de 20 anos: {usuarios_below_20}')
+
 print(f'Número de usuários acima de 40 anos: {usuarios_above_40}')
+
+print(f'O gênero do usuário mais jovem é: {youngest_genero}')
