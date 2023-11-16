bcd4aca1c40c6b8abaaf70658385f2fade7a8983
Alves Pinho <108952262+alvespinho@users.noreply.github.com>
2023-11-16 01:44:25 -0300

    Add files via upload

---
+++ PY_PROVA6.py
@@ -0,0 +1,12 @@
+list1 = input("Insira uma lista de números separados por espaço: ").split()
+list2 = input("Insira outra lista de números  separados por espaço: ").split()
+
+list1 = [int(num) for num in list1]
+list2 = [int(num) for num in list2]
+
+comum = list(set(list1) & set(list2))
+soma_comum = sum(comum)
+
+resultado = (comum, soma_comum)
+
+print(resultado)
