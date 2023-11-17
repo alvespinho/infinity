e540b05f87bfcbfa3df2cb648051313c01be7d26
Alves Pinho <108952262+alvespinho@users.noreply.github.com>
2023-11-17 08:08:54 -0300

    Update LP_PROVA5.py

--- LP_PROVA5.py
+++ LP_PROVA5.py
@@ -3,33 +3,33 @@ soma_idades = 0
 usuarios_below_20 = 0
 usuarios_above_40 = 0
 oldest_usuario = None
-oldest_idade = 0 ####Referência de Validação 
+oldest_idade = 0
 youngest_genero = None
-youngest_idade = 100 ####Referência de Validação
+youngest_idade = 120 #### Idade Referência 
 
 for i in range(5):
-    usuario = input(f'Digite o nome do Usuário #{i+1}: ')
-    idade = int(input(f'Digite a idade do Usuário #{i+1}: '))
-    genero = input(f'Digite o gênero do Usuário #{i+1}: ')
+	usuario = input(f'Digite o nome do Usuário #{i+1}: ')
+	idade = int(input(f'Digite a idade do Usuário #{i+1}: '))
+	genero = input(f'Digite o gênero do Usuário #{i+1}: ')
     
-    soma_idades += idade
-    dados.append({'Usuario': usuario, 'Idade': idade, 'Genero': genero})
+	soma_idades += idade
+	dados.append({'Usuario': usuario, 'Idade': idade, 'Genero': genero})
     
-    #### Atualizar mais velho
-     if idade > oldest_idade:
-    	   oldest_idade = idade 
-    	   oldest_usuario = usuario
+#### Atualizar valor da pessoa mais velha
+	if idade > oldest_idade:
+ 		oldest_idade = idade
+ 		oldest_usuario = usuario
     
-    #### Atualizar caçula 
-    if idade < youngest_idade:
-        youngest_idade = idade
-        youngest_genero = genero
+#### Atualizar valor da idade caçula 
+	if idade < youngest_idade:
+		youngest_idade = idade
+		youngest_genero = genero
 
-    #### Conta quantos usuários abaixo de 20 e 		#### acima de 40
-    if idade < 20:
-        usuarios_below_20 += 1
-    elif idade > 40:
-        usuarios_above_40 += 1
+#### Qnts acima de 40, abaixo de 20
+	if idade < 20:
+		usuarios_below_20 += 1
+	elif idade > 40:
+		usuarios_above_40 += 1
 
 media_idades = int(soma_idades / len(dados))
 print(f'A média de idades é: {media_idades}')
