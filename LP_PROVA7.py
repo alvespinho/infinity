a7892b1470e0e3fa884716b03c68436aa15bbc2c
Alves Pinho <108952262+alvespinho@users.noreply.github.com>
2023-11-24 11:08:38 -0300

    Add files via upload

---
+++ LP_PROVA7.py
@@ -0,0 +1,13 @@
+soma = []
+num = int (input('Digite os números para análise (0 finaliza): '))
+
+while num != 0:
+    soma.append(num)
+    num = int (input('Digite os números para análise (0 finaliza): '))
+    contagem = len (soma)
+    total = sum(soma)
+    media = total / len(soma)
+    if num == 0:
+        print (f'   A quantidade de números listados: {contagem}')
+        print (f'   O valor total dos números listados é: {total}')
+        print (f'   A média dos números listados é: {int (media)}')
