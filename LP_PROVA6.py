1659d57069aa73e2a5f4cc3b955b3d390df923cf
Alves Pinho <108952262+alvespinho@users.noreply.github.com>
2023-11-24 09:35:40 -0300

    Add files via upload

---
+++ LP_PROVA6.py
@@ -0,0 +1,15 @@
+bingo = 7
+num = int(input('Adivinhe o número da máquina: '))
+tentativas = 1
+
+while num != bingo:
+    if num < bingo:
+        print('O número é maior. Tente novamente.')
+    else:
+        print('O número é menor. Tente novamente.')
+    
+    num = int(input('Adivinhe o número da máquina: '))
+    tentativas += 1
+
+print(f'Parabéns! Você acertou em {tentativas} tentativas.')
+
