8c13e8560280c160d553a7fb87d843291f0c1685
Alves Pinho <108952262+alvespinho@users.noreply.github.com>
2023-12-07 18:41:45 -0300

    Update prova7.py

--- prova7.py
+++ prova7.py
@@ -1,27 +1,22 @@
 alunos_matriculados = []
 
-
 def add_aluno(aluno, matricula):
     alunos_matriculados.append({'Aluno': aluno, 'Matrícula': matricula})
     print(f'{aluno.upper()} foi adicionado ao Sistema! Matrícula de número: {
           matricula}')
 
-
 def del_aluno(aluno):
     global alunos_matriculados
-
     for item in alunos_matriculados:
         if item['Aluno'] == aluno:
             alunos_matriculados.remove(item)
             print(f'{aluno.upper()} foi removido do Sistema!')
             break
 
-
 def ver_alunos():
     print('\n### SISTEMA DE MATRÍCULAS ####')
     for item in alunos_matriculados:
         print(f"{item['Aluno']} | {item['Matrícula']}")
-
 
 while True:
     print('\n### SISTEMA DE MATRÍCULAS ###')
@@ -36,16 +31,12 @@ while True:
         aluno = input('Aluno: ')
         matricula = int(input('Nº de Matrícula: '))
         add_aluno(aluno, matricula)
-
     elif escolha == '2':
         aluno = input('Qual aluno deseja remover?  ')
         del_aluno(aluno)
-
     elif escolha == '3':
         ver_alunos()
-
     elif escolha == '4':
         break
-
     else:
         print('Ops! Opção inválida. Tente novamente.')
