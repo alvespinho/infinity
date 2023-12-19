4266a299b60378a5d9b6ccf2cc5d034d0a30ca92
Alves Pinho <108952262+alvespinho@users.noreply.github.com>
2023-12-19 17:20:56 -0300

    Add files via upload

---
+++ prova9.py
@@ -0,0 +1,32 @@
+### FUNÇÕES
+def verificar(media):
+    if media == 10:
+        return (f"Parabéns, sua média é 10")
+    elif media < 7: 
+        return (f"Reprovado")
+    elif media >= 7: 
+        return (f"Parabéns, você está aprovado!")
+
+
+def calcular(nota1, nota2, nota3, nota4, nota5):
+    return (nota1 + nota2 + nota3 + nota4 + nota5)/ 5
+    
+
+#### APP
+
+nota1 = float (input('Digite Nota 1: '))
+nota2 = float (input('Digite Nota 2: '))
+nota3 = float (input('Digite Nota 3: '))
+nota4 = float (input('Digite Nota 4: '))
+nota5 = float (input('Digite Nota 5: '))
+media = calcular(nota1, nota2, nota3, nota4, nota5)
+
+situacao = verificar(media)
+print(media)
+print(situacao)
+
+
+
+
+
+
