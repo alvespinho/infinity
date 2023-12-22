0278a96ae193a6747668abf44f1c730eb73149fb
Alves Pinho <108952262+alvespinho@users.noreply.github.com>
2023-12-22 03:01:12 -0300

    Add files via upload

---
+++ prova12.py
@@ -0,0 +1,16 @@
+def maior_numero (n1, n2):
+    if n1 > n2:
+        return n1
+    else:
+        return n2
+    
+n1 = float(input('Número 1: '))
+n2 = float(input('Número 2: '))
+
+
+### FUNÇÃO CONDICIONAL
+result = maior_numero(n1, n2)
+print (f'Aqui, {result} é o maior número.')
+
+### FUNÇÃO BUILT-IN MAX
+print (f'E aqui, {max(n1, n2)} também.')
